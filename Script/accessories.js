64ef7d455c349cad65c641d451d1a1e8bd386bde
theconcealer <mndubuisi1000@gmail.com>
2025-11-28 13:51:19 +0100

    Add files via upload

---
+++ Script/accessories.js
@@ -0,0 +1,7 @@
+var ReloaderContainer = document.getElementById('ReloaderContainer');
+
+function RemoveReloader (){
+ReloaderContainer.style.display = 'none'   
+}
+
+setTimeout(RemoveReloader, 1700)
---
+++ Script/men.js
@@ -0,0 +1,211 @@
+$('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 3,
+            nav: false
+        },
+        1000: {
+            items: 4,
+            nav: true,
+
+        }
+    }
+})
+
+
+// Classics Spotlight Section
+
+// FirstIMG 
+
+function ChangeFirstIMGa() {
+    var FirstIMG = document.getElementById('FirstIMG');
+    FirstIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/lkwfba88t6qix4qxaavi/NIKE+ZOOM+VOMERO+5.png';
+}
+
+function ChangeFirstIMGb() {
+    var FirstIMG = document.getElementById('FirstIMG');
+    FirstIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/24d898c4-0400-457e-be38-fd9d9b36e4e2/NIKE+ZOOM+VOMERO+5.png';
+}
+
+
+function ChangeFirstIMGc() {
+    var FirstIMG = document.getElementById('FirstIMG');
+    FirstIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/71e79773-61aa-46f2-baec-e78ee8063edb/NIKE+ZOOM+VOMERO+5.png';
+}
+
+// SecondIMG
+
+function ChangeSecondIMGa() {
+    var SecondIMG = document.getElementById('SecondIMG');
+
+    SecondIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f8628962-8b8e-4676-8ff6-0a0c5f9c0930/W+NIKE+ZOOM+VOMERO+5.png';
+}
+
+function ChangeSecondIMGb() {
+    var SecondIMG = document.getElementById('SecondIMG');
+
+    SecondIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/2b9988ff-ed06-407f-a5f6-c29a74770eb1/W+NIKE+ZOOM+VOMERO+5.png';
+}
+
+function ChangeSecondIMGc() {
+    var SecondIMG = document.getElementById('SecondIMG');
+
+    SecondIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/87642fa5-5ba3-4719-abc3-bbc2757500da/W+NIKE+ZOOM+VOMERO+5.png';
+}
+
+// ThirdIMG
+
+
+function ChangeThirdIMGa() {
+    var ThirdIMG = document.getElementById('ThirdIMG');
+    ThirdIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a73adc66-c005-4652-91e2-3e2d2b0eb46a/NIKE+CORTEZ.png'
+}
+
+function ChangeThirdIMGb() {
+    var ThirdIMG = document.getElementById('ThirdIMG');
+    ThirdIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/9ab8e739-b9e7-4ce9-b071-cb7db0535b99/NIKE+CORTEZ.png'
+}
+
+function ChangeThirdIMGc() {
+    var ThirdIMG = document.getElementById('ThirdIMG');
+    ThirdIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/0a2a36de-c068-4278-aecb-c3a0ddf48bf4/NIKE+CORTEZ.png'
+}
+
+// FourthIMG
+
+
+function ChangeFourthIMGa() {
+    var FourthIMG = document.getElementById('FourthIMG');
+    FourthIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/6fa90dff-89e1-410a-b9bb-8a26565b9e55/NIKE+AIR+HUARACHE+RUNNER.png'
+}
+
+function ChangeFourthIMGb() {
+    var FourthIMG = document.getElementById('FourthIMG');
+    FourthIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c8f09fff-58b9-4b8c-9d05-90334be9a69d/NIKE+AIR+HUARACHE+RUNNER.png'
+}
+
+function ChangeFourthIMGc() {
+    var FourthIMG = document.getElementById('FourthIMG');
+    FourthIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e06c59cf-a906-4e67-a6bc-b8245da588a9/NIKE+AIR+HUARACHE+RUNNER.png'
+}
+
+
+// FifthIMG
+
+function ChangeFifthIMGa() {
+    var FifthIMG = document.getElementById('FifthIMG');
+    FifthIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/ece5296b-a7d1-494d-89a4-5199cdf1361a/NIKE+DUNK+LOW+RETRO.png'
+}
+
+function ChangeFifthIMGb() {
+    var FifthIMG = document.getElementById('FifthIMG');
+    FifthIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/d6242443-257d-40af-8f4f-deba3d496eb5/NIKE+DUNK+LOW+RETRO.png'
+}
+
+function ChangeFifthIMGc() {
+    var FifthIMG = document.getElementById('FifthIMG');
+    FifthIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4329d447-d48e-4886-8585-b73355c52cda/NIKE+DUNK+LOW+RETRO.png'
+}
+
+
+// SixthIMG
+
+
+function ChangeSixthIMGa() {
+    var SixthIMG = document.getElementById('SixthIMG');
+    SixthIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/45b70a21-b1c3-4088-b008-acb6cc433b71/GIANNIS+FREAK+6.png'
+}
+
+function ChangeSixthIMGb() {
+    var SixthIMG = document.getElementById('SixthIMG');
+    SixthIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a6955769-6091-49d0-857e-ecebd21a0850/GIANNIS+FREAK+6NRG.png'
+}
+
+
+function ChangeSixthIMGc() {
+    var SixthIMG = document.getElementById('SixthIMG');
+    SixthIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/91cf9c6e-0803-40ea-baa4-558ca4ecc3a2/GIANNIS+FREAK+6.png'
+}
+
+
+// SeventhIMG
+function ChangeSeventhIMGa() {
+    var SeventhIMG = document.getElementById('SeventhIMG');
+    SeventhIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/c0e9acf7-e301-4a9f-be2c-b521ca9e9052/JORDAN+TRUE+FLIGHT.png'
+}
+
+function ChangeSeventhIMGb() {
+    var SeventhIMG = document.getElementById('SeventhIMG');
+    SeventhIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/86b141f7-a4e9-42c7-ac31-21d83ae8b3c5/JORDAN+TRUE+FLIGHT.png'
+}
+
+function ChangeSeventhIMGc() {
+    var SeventhIMG = document.getElementById('SeventhIMG');
+    SeventhIMG.src = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/fd9a72a1-6579-43fd-803e-ee32da567fc5/JORDAN+TRUE+FLIGHT.png'
+}
+
+
+
+
+
+// Display ShowShop Div 
+
+var ShowShop = document.getElementById('ShowShop');
+
+var ShowShopChild = document.getElementById('ShowShopChild');
+
+function ChooseShop() {
+    ShowShop = document.getElementById('ShowShop');
+
+
+    ShowShop.style.display = 'block'
+}
+
+function DelayDisplay() {
+
+    location.reload();
+
+
+}
+
+function xmark() {
+
+
+
+    ShowShopChild.style.animationName = 'MoveShowShop2';
+    ShowShopChild.style.animationDuration = '0.5s'
+
+
+
+    setTimeout(DelayDisplay, 600)
+
+
+}
+
+// Reloader
+
+var ReloaderContainer = document.getElementById('ReloaderContainer');
+
+function RemoveReloader (){
+ReloaderContainer.style.display = 'none'   
+}
+
+setTimeout(RemoveReloader, 1700)
+
+
+
+
+
+
+
+
+
+
+
